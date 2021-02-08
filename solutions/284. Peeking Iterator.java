a9a5588349d5e2481eb5ef74f1fe344ce7318bfd
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-02-08 22:31:39 +0530

    4 ms

---
+++ solutions/284. Peeking Iterator.java
@@ -0,0 +1,43 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+​
+class PeekingIterator implements Iterator<Integer> {
+    private Iterator<Integer> it;
+    private boolean hasPeeked;
+    private Integer peekedElm;
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        it=iterator;
+        hasPeeked=false;
+        peekedElm=null;
+        
+        
+    }
+    
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        if(hasPeeked){
+            return peekedElm; 
+        }
+        peekedElm=it.next();
+        hasPeeked=true;
+        return peekedElm;
+    }
+    
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        if(hasPeeked){
+            hasPeeked=false;
+            
+            return peekedElm; 
+        }
+        return it.next();
+    }
+    
+    @Override
+    public boolean hasNext() {
+        return hasPeeked || it.hasNext();
+    }
+}
