07f0c66b67b09840a3e2854d7b36146f02d2d40b
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-03-08 22:56:53 +0530

    0 ms

---
+++ solutions/1332. Remove Palindromic Subsequences.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        if(s==null || s.equals("")) return 0;
+        int start=0;
+        int end=s.length()-1;
+        while(start<end){
+            if(s.charAt(start)!=s.charAt(end)) return 2;
+            start++;
+            end--;
+        }
+        return 1;
+    }
+}
