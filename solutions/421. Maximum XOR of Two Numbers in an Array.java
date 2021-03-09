f0ef1cdcf45703333783a9bad8eeb93d5105b784
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-03-10 02:00:46 +0530

    30 ms

--- solutions/421. Maximum XOR of Two Numbers in an Array.java
+++ solutions/421. Maximum XOR of Two Numbers in an Array.java
@@ -1,5 +1,14 @@
 class Solution {
+    class TrieNode{
+        TrieNode[] childs;
+       
+        TrieNode(){
+            childs=new TrieNode[2];
+        }
+    }
+    TrieNode root=new TrieNode();
     public int findMaximumXOR(int[] nums) {
+        /*
        int max=0,mask=0;
         
         for(int i=31;i>=0;i--){
@@ -7,16 +16,25 @@ class Solution {
             Set<Integer> set=new HashSet<Integer>();
             for(int num : nums){
                 set.add(mask&num);
-            }
+            }
             int tmp= max | (1<<i);
             for(int x : set){
                 if(set.contains(x^tmp)){ //a^b=c means b=a^c
                     max=tmp;
                     break;
-                }
-            }
+                }
+            }
+        }
+        return max;
+        
+        */
+        /* using trie */
+        for(int num : nums)
+        {
+            insert(num);
+        }
+        int max=Integer.MIN_VALUE;
+        for(int num :nums){
+            max=Math.max(max,findMaxXor(num));
         }
         return max;
-       
-    }
-}
