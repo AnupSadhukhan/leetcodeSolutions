c8fa82c0cd34a5f28d26a3f9f8e7a6a00578b631
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-03-13 03:06:15 +0530

    0 ms

--- solutions/779. K-th Symbol in Grammar.java
+++ solutions/779. K-th Symbol in Grammar.java
@@ -1,8 +1,8 @@
 class Solution {
     public int kthGrammar(int N, int k) {
-        if(N==2){
+      /*  if(N==2){
             return k==1?0:1;
-        }
+        }
         if(N<2) return 0;
         int k1=k;
         k=k%2==0?k/2:(k/2)+1;
@@ -12,18 +12,23 @@ class Solution {
             if(k1%2==0){
                // System.out.println("1. 0 even =1");
                 return 1;
-            }
+            }
              //System.out.println("2. 0 odd =0");
             return 0;
-        }
+        }
         else{
             if(k1%2==0){
                  //System.out.println("3. 1 even =0");
                 return 0;
-            } 
+            } 
             //System.out.println("4. 1 odd =1");
             
             return 1;
-        }
+        }
+        */
+        /* another approch */
+        //return Integer.bitCount(k-1)%2==0?0:1;
+        //approch 3
+        return Integer.bitCount(k-1)&1;
     }
 }
