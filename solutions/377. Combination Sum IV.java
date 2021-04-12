69cfe0b60fbf849a580fd383be50ea8e8301f3f4
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-04-13 04:46:07 +0530

    1 ms

---
+++ solutions/377. Combination Sum IV.java
@@ -0,0 +1,33 @@
+class Solution {
+    //https://leetcode.com/problems/combination-sum-iv/discuss/85036/1ms-Java-DP-Solution-with-Detailed-Explanation
+    // This problem is similar to coin change II. but in that problem order didn't matter
+    // where as in this problem, diff order with same combination is counter as different. so, we
+    // have tweak the solution a little bit. to count diff order. so, we will make an dp with size of target+1, for
+    // sum 0, combination is always 1, then for 2..target we will iterate over the all elements. if for a elements
+    // there is some combination that means for that sum we can make that many possible of combination. so we will
+    // count that valus with for current sum..
+    // e.g. arr = [1,2,3]; target=4
+    // dp=[1,0,0,0,0];
+    //sum = 1 elm = 1..2..3, dp=[1,1,0,0,0]
+    // sum =2 elm = 1..2..3, dp=[1,1,2,0,0] {(1,1),(2)} , 
+    // sum=3 elm = 1  dp=[1,1,2,2,0], elm =2, dp=[1,1,2,3,0], elm=3, dp=[1,1,2,4,0]
+    // sum=4 elm = 1  dp=[1,1,2,4,4], elm =2, dp=[1,1,2,4,6], elm=3, dp=[1,1,2,4,7] 
+    //rule: dp[sum]=dp[sum-elm1]+dp[sum-elm2]+dp[sum-elm3]... for all elements if(sum-elm>=0)
+    
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp=new int[target+1];
+        dp[0]=1;
+       
+            for(int sum=1;sum<=target;sum++){
+                for(int i=0;i<nums.length;i++){
+                    if(sum-nums[i]>=0){ 
+                        dp[sum]+=dp[sum-nums[i]];
+                    }
+                }
+                
+            }
+        return dp[target];
+        
+        
+    }
+}
