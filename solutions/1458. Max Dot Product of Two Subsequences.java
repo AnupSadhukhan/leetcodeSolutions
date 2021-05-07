3637a1cac1077d41926e09bcee6bb6f90a324574
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-05-08 02:10:04 +0530

    10 ms

--- solutions/1458. Max Dot Product of Two Subsequences.java
+++ solutions/1458. Max Dot Product of Two Subsequences.java
@@ -1,4 +1,5 @@
 class Solution {
+    //use LCS
     public int maxDotProduct(int[] nums1, int[] nums2) {
         int[][] dp=new int[nums1.length][nums2.length];
         for(int i=0;i<nums1.length;i++){
