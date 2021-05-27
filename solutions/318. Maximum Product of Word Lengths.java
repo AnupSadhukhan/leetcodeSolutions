88831d530e07f23a1b81c3171200f14de9620d0d
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-05-28 02:21:26 +0530

    8 ms

--- solutions/318. Maximum Product of Word Lengths.java
+++ solutions/318. Maximum Product of Word Lengths.java
@@ -1,28 +1,3 @@
-class Solution {
-    public int maxProduct(String[] words) {
-        int n=words.length;
-        int max=0;
-        List<boolean[]> list=new ArrayList<>();
-        for(int i=0;i<n;i++){
-           
-            boolean[] arr=new boolean[26];
-            int count=0;
-            int j=0;
-            char[] chs=words[i].toCharArray();
-            while(count!=26 && j<chs.length){
-                if(!arr[chs[j]-'a']){
-                    arr[chs[j]-'a']=true;
-                    count++;
-                }
-                j++;
-             }
-             list.add(arr);
-        }
-        for(int i=0;i<words.length-1;i++){
-          
-            boolean[] arr1=list.get(i);
-            int len1=words[i].length();
-            for(int j=i+1;j<n;j++){
                 boolean arr2[]=list.get(j);
                 boolean flag=false;
                 int len2=words[j].length();
@@ -30,10 +5,31 @@ class Solution {
                     if(arr1[k] && arr2[k]){
                         flag=true;
                         break;
-                    }
-                }
+                    }
+                }
                 if(!flag){
                     max=Math.max(max,len1*len2);
+                }
+            }
+        }
+        return max;*/
+        // using bit manipulation
+        int[] values=new int[n];
+        for(int i=0;i<n;i++){
+            int value=0;
+            for(char c :  words[i].toCharArray()){
+                value |= 1<<(c-'a');
+            }
+            values[i]=value;
+        }
+        for(int i=0;i<n-1;i++){
+            int len1=words[i].length();
+            for(int j=i+1;j<n;j++){
+                int len2=words[j].length();
+                if((values[i] & values[j])==0){
+                   
+                    max=Math.max(max,len1*len2);
+                    
                 }
             }
         }
