98f8649b29bc05f9b85491807c6a687ce0dbfeed
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-03 01:48:04 +0530

    0 ms

--- solutions/767. Reorganize String.java
+++ solutions/767. Reorganize String.java
@@ -1,5 +1,14 @@
 class Solution {
-   
+   /* map chars to 26 char array and count frequecy of every chars
+     also keep a track of higest frequency and char.
+     if number of other chars is less max-1 return empty string because 
+     we can not arrange, e.g. aaab can not be arranged, aab can be arrange to aba
+     
+     else  we will place higest frequency char in alternate position i.e. index+2 pos
+     once it is done, we take all other char and put them alter gap or blank position
+     if index reaches end of char array, initialize it to 1 because we have to max freq
+     char at 0,2,4,..
+     */     
     public String reorganizeString(String s) {
         if(s==null || s.equals("")) return "";
         int[] letter=new int[26];
@@ -13,7 +22,6 @@ class Solution {
             }
             
         }
-        //System.out.println("max"+max+" maxIndex "+maxIndex);
         if(s.length()-max<max-1) return "";
         int idx=0;
         char[] arr=new char[s.length()];
