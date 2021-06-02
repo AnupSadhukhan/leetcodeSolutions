bddd9f21aa7babcc512a552160c92d2a7153a026
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-03 01:35:20 +0530

    3 ms

--- solutions/135. Candy.java
+++ solutions/135. Candy.java
@@ -1,4 +1,29 @@
 class Solution {
+    /*  we will keep track of last higest rating children or peak in graph( if at any
+        sccenario we have to increase hight, then left childrens of last highest rating or peak 
+        will not have any impact. we will keep track of candy given to last children, last higest
+        rating children and candy given to last higest rating children i.e. last peak)
+        case I : rating of cur children is greater than previous
+            we will give one more extra candy than last children
+            update last higest rating position
+            update candy given to last highest rating children
+            update candy given to last children irrespective of rating
+        case II : rating of cur children is less than previous one
+            to minimize candy distribution we will alway try to give 1 candy here
+            if there many children between last higest rating children and cur and
+            we have to give one extra candy to all of them to maintain condition
+            if last higest rating children and next to last highest rating children or peak
+            hold same number of candies, we have to give one candy to last higest rating or peak
+            children.
+            update candy given
+            update candy given to last children irrespective of rating
+        case III : rating of cur children is same as last children
+            we will give 1 candy to minimize distribution and maintain condition
+            update update last higest rating position to cur bcz this children too
+            hold same rating,
+            update candy given to last highest rating children
+            update candy given to last children irrespective of rating
+    */
     // if rating is greater than the previous children then we will add
     public int candy(int[] ratings) {
         if(ratings.length==0) return 0;
@@ -34,6 +59,7 @@ class Solution {
            
         }
         return candy;
+        
         /*int[] candy=new int[ratings.length];
         int lastPos=0;
         int lastCandy=1;
@@ -43,3 +69,11 @@ class Solution {
                 candy[i]=lastCandy+1;
                 lastCandy++;
                 lastPos=i;
+            }
+            if(ratings[i]<ratings[i-1]){
+                candy[i]=1;
+                lastCandy=1;
+                int dist=i-lastPos;
+                 candy[i]+=dist-1;
+                if(candy[lastPos]<=dist)
+                   candy[lastPos]+=1;
