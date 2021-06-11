2e56264183411fc17078cb4bf31a9edeef8803b1
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-11 22:06:32 +0530

    1 ms

--- solutions/238. Product of Array Except Self.java
+++ solutions/238. Product of Array Except Self.java
@@ -1,4 +1,14 @@
 class Solution {
+    /* if there are more than one zero then all elements will become zero,
+     because at to the left or right there will be atleast one zero hence
+      product will become zero, so in this case we will simply return newly created 
+      result array without any modification else if there is only one zero,
+      then all elements will become zero except product at that index will be
+      product of left elems and product of right elems.
+      if there there is no zero elements then we will calculate products of all elems
+      and result array's elems will be (product of all elems/element) and return this 
+      result
+      */
     public int[] productExceptSelf(int[] nums) {
               
         int product=1;
