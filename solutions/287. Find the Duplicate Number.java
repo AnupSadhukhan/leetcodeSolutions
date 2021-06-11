e1c5ec7a2a0cce6735c378c8d78c70360559017f
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-11 22:15:43 +0530

    5 ms

--- solutions/287. Find the Duplicate Number.java
+++ solutions/287. Find the Duplicate Number.java
@@ -1,4 +1,8 @@
 class Solution {
+    // we can use sorting and set to check duplicate, but if we 
+    // follow the constraint then we have to solve it using 
+    // floyd's tortoise and hare (cycle detection) algo
+    // check approch 3 in the solution for better understanding
     public int findDuplicate(int[] nums) {
         // 30ms
       /* Arrays.sort(nums);
@@ -6,7 +10,7 @@ class Solution {
             if(nums[i]==nums[i-1]) return nums[i];
         }
         return 0;*/
-        
+        //5ms
         int slow=nums[0];
         int fast=nums[0];
         do{
