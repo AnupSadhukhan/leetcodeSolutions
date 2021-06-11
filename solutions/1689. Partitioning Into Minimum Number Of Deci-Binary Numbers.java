97f68596c547ebbea40b570c647ea6eaeb5aae0e
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-11 21:21:36 +0530

    4 ms

--- solutions/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
+++ solutions/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
@@ -1,4 +1,13 @@
 class Solution {
+    /*  Approch 1: brute force
+        if the char is not zero, reduce by 1 and if it is zero reduce by zero
+        do the same for whole string repeatedly until string is left with zero only
+        that n="32" => "11" +"11" +"10"
+        Approch 2:
+        from above approch we can see that loop is running at most 3 time,
+        which is max value in the string, so we can reduce the process just 
+        by iterating only once and finding max char, and it will be our ans
+    */
     public int minPartitions(String n) {
         char[] chs=n.toCharArray();
         // 33 ms
