5980239b7ea810f47c26a7caa7fae0ea1eac50c7
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-20 17:25:29 +0530

    37 ms

--- solutions/128. Longest Consecutive Sequence.java
+++ solutions/128. Longest Consecutive Sequence.java
@@ -1,3 +1,57 @@
+class Solution {
+    /* initialize window size as 1 every time then check 
+    and run while curNum-1 is present in the left side, incrase window size,
+    run while curNum+1 is presnt in the right size, increase window size,
+    update result as max of window size;
+    return max result;
+    */
+    //O(n) 5ms
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> input = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();        
+        int curr, window, result = 0;
+        
+        for (int num: nums) input.add(num);
+        
+        for (int num: input) {
+            if (visited.contains(num)) continue;
+            
+            window = 1;
+            
+            // go left;
+            curr = num-1;
+            while (input.contains(curr)) {
+                window++;
+                visited.add(curr);
+                curr--;
+            }
+                
+            // go right
+            curr = num+1;
+            while (input.contains(curr)) {
+                window++;
+                visited.add(curr);
+                curr++;                
+            }
+            
+            result = Math.max(window, result);
+        }
+        
+        return result;
+    }
+   /* 35ms O(n)
+   public int longestConsecutive(int[] nums) {
+        if(nums.length==0) return 0;
+        if(nums.length==1) return 1;
+        Map<Integer,int[]> map=new HashMap<>();
+        int max=0;
+        for(int num : nums){
+            if(map.get(num)==null){
+                int[] up=map.get(num+1);
+                int[] down=map.get(num-1);
+                if(up!=null && down!=null){
+                    int[] dist=new int[]{1};
+                    dist[0]+=up[0];
                     dist[0]+=down[0];
                     
                     
@@ -8,22 +62,3 @@
                 }
                 else if(up==null && down!=null){
                     down[0]++;
-                    map.put(num,down);
-                    max=Math.max(max,down[0]);
-                }
-                else if(down==null && up!=null){
-                    up[0]++;
-                    map.put(num,up);
-                    max=Math.max(max,up[0]);
-                }
-                else{
-                    map.put(num,new int[]{1});
-                    max=Math.max(max,1);
-                }
-            
-            }
-            
-        }
-        return max;
-    }*/
-}
