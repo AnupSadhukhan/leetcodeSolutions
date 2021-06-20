5a6f05e49cdcb00b7037f72d7e722c07d48359b2
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-20 20:39:55 +0530

    93 ms

--- solutions/307. Range Sum Query - Mutable.java
+++ solutions/307. Range Sum Query - Mutable.java
@@ -7,14 +7,12 @@ class NumArray {
         len=2*len;
         st=new int[4*nums.length+1];
         arr=nums;
-       // System.out.println(" len "+nums.length);
+     
         constructTree(0,0,nums.length-1);
-        //print("construct - ");
+      
     }
     private int constructTree(int root,int ss,int se ){
         if(ss==se){
-          //System.out.println(" root "+root+" "+ss);;
-​
             st[root]=arr[ss];
             return st[root];
         }
@@ -32,3 +30,34 @@ class NumArray {
             updateUtil(index,diff,mid+1,se,2*root+2);
         }
         
+        
+    }
+    private int sumRangeUtil(int root,int ss,int se,int ql,int qr){
+        if(qr<ss || ql>se) return 0;
+        if(ql<=ss && qr>=se){
+            return st[root];
+        }
+        int mid=(ss+se)/2;
+        int sum=sumRangeUtil(2*root+1,ss,mid,ql,qr)+sumRangeUtil(2*root+2,mid+1,se,ql,qr);
+        return sum;
+    }
+    public void update(int index, int val) {
+         int diff=val-arr[index];
+        arr[index]=val;
+         updateUtil(index,diff,0,arr.length-1,0);
+    }
+    
+    public int sumRange(int left, int right) {
+        int x=sumRangeUtil(0,0,arr.length-1,left,right);
+        return x;
+        
+    }
+   
+}
+​
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
