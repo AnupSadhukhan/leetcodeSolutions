eb3706297cfc8aa3ca6c3dd7685a297952f7be36
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-20 19:02:03 +0530

    41 ms

--- solutions/729. My Calendar I.java
+++ solutions/729. My Calendar I.java
@@ -1,4 +1,17 @@
 class MyCalendar {
+    /* there can be three case
+    case 1: new event is partially overlapping at left side with some other event which we have added previously
+    case 2: new event is partially overlapping at right side with some other event which we have added before
+    case 3: new event is totally with other event
+    case 4: new event is outside i.e. not overlapping with other events
+    
+    if there is no overlap, we can insert or book the events else not, use treeset which we 
+    store events according to start time of event, we will find ceil and floor whenever
+    we are trying to book a new event, if floor events end time is more then new events start time
+    or if ceil events start time is less than new events start time, we can not book the event 
+    hence return false else add the event to treeset and return the result
+    */
+    
     private TreeSet<int[]> events;
     public MyCalendar() {
         events=new TreeSet<int[]>((int[] a,int[] b)->a[0]-b[0]);
