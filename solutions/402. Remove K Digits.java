b06fa68c5b3d1ef9cc9c9ffd8f4585f47cab4cbe
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-20 19:51:14 +0530

    4 ms

--- solutions/402. Remove K Digits.java
+++ solutions/402. Remove K Digits.java
@@ -1,4 +1,18 @@
 class Solution {
+    /* if n == k return 0
+    else if there is smaller digit after a digit then if we will delete greater
+    digit, smaller digit will replace the position and give us less number, so
+    we will delete higher digit as well as decrease k and i beacuse after deleting position
+    has shifted one side to the left,
+    we will do this process until we process till the end,
+    after end if still k is greater than zero we have to remove digit from end
+    until k becomes, because after processing the whole num i.e. deleting higer
+    num if next nums is smaller, our new num will be in increasing sequence, so num
+    higher number will reside at the end, so to minimize the value delete from end.
+    
+    once k is zero, check for leading zero and delete them as well after that return the num.
+    */
+    
     public String removeKdigits(String num, int k) {
        int n=num.length();
         if(n==k) return "0";
