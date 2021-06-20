f51125103ea64c40e82f4b2d0cb5e95751a0c554
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-20 20:12:19 +0530

    1 ms

--- solutions/1094. Car Pooling.java
+++ solutions/1094. Car Pooling.java
@@ -1,4 +1,21 @@
 class Solution {
+    /* Approch 1 O(nlogn): sort trips as per start location followed by end location
+        use priorityqueue to keep track of person entering to car along with their destination
+        before starting a trip check runnng while, whether there are passengers whose destination already been passed, then 
+        release their seat, if at any point capacity or seat is less than passengers return false
+        as we can not do this car pooling else reserve seat for those passengers i.e. decarese seat or capacity
+        add passegters and destination to queue.
+        
+        if we can complete all trips return true else false;
+        
+        approch 2 O(n) : 
+          use a array, for every trip increase value by passengers at index start trip location
+          and decrease value by passengers at index of end trip location,
+          after processing do
+          capacity-=value of array 
+          if at any index value is negative, that means all trips can not be ompleted
+          */
+    
    /* public boolean carPooling(int[][] trips, int capacity) {
         Arrays.sort(trips,(a,b)->{
             if(a[1]==b[1]) return a[2]-b[2];
@@ -38,5 +55,5 @@ class Solution {
             if(capacity < 0) return false;
         }
         return true;
-    }
+    }
 }
