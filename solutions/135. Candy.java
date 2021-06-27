f8f60037ffb983f7da899b74f151e4c63d702a54
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-06-28 00:34:19 +0530

    2 ms

--- solutions/135. Candy.java
+++ solutions/135. Candy.java
@@ -1,5 +1,3 @@
-class Solution {
-    /*  we will keep track of last higest rating children or peak in graph( if at any
         sccenario we have to increase hight, then left childrens of last highest rating or peak 
         will not have any impact. we will keep track of candy given to last children, last higest
         rating children and candy given to last higest rating children i.e. last peak)
@@ -24,56 +22,53 @@ class Solution {
             update candy given to last highest rating children
             update candy given to last children irrespective of rating
     */
-    // if rating is greater than the previous children then we will add
-    public int candy(int[] ratings) {
-        if(ratings.length==0) return 0;
-        int candy=1; // it will hold min num of candy
-        int lastPosCandy=1; // lastPoscandy is the last peak position
-        int lastPos=0;
-        int lastCandy=1;
-        for(int i=1;i<ratings.length;i++){
-            if(ratings[i]>ratings[i-1]){
-                candy+=lastCandy+1;
-                lastPosCandy=lastCandy+1;
-                lastCandy++;
-                lastPos=i;
-                
-            }
-            if(ratings[i]<ratings[i-1]){
-                candy+=1;
-                lastCandy=1;
-                int dist=i-lastPos;
-                candy+=dist-1;
-                if(lastPosCandy<=dist){
-                    lastPosCandy+=1;
-                    candy++;
-                }
-                   
-            }
-            if(ratings[i]==ratings[i-1]){
-                candy+=1;
-                lastCandy=1;
-                lastPosCandy=1;
-                lastPos=i;
-            }
-           
-        }
-        return candy;
-        
-        /*int[] candy=new int[ratings.length];
-        int lastPos=0;
-        int lastCandy=1;
-        candy[0]=1;
-        for(int i=1;i<ratings.length;i++){
-            if(ratings[i]>ratings[i-1]){
-                candy[i]=lastCandy+1;
-                lastCandy++;
-                lastPos=i;
+    /* 
+        //1ms 2 pass O(n)
+    
+       int[] candy = new int[ratings.length];
+        for (int i = 0; i < candy.length; i++) {
+            candy[i] = 1;
+        }
+​
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candy[i] = candy[i - 1] + 1;
             }
-            if(ratings[i]<ratings[i-1]){
-                candy[i]=1;
+        }
+​
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candy[i] = Math.max(candy[i],candy[i + 1] + 1);
+            }
+        }
+​
+        int count = 0;
+        for (int i = 0; i < candy.length; i++) {
+            count += candy[i];
+        }
+​
+        return count;
+    */
+    
+    //2ms single pass o(1) space
+     int lastPeakPos=0;
+        int lastCandy=1;
+        int totalCandy=1;
+        int lastPeakCandy=1;
+        for(int i=1;i<ratings.length;i++){
+            if(ratings[i]>=ratings[i-1]){
+                if(ratings[i]==ratings[i-1]){
+                    lastCandy=1;
+                }
+                else{
+                    lastCandy+=1;
+                }
+                totalCandy+=lastCandy;
+                lastPeakPos=i;
+                lastPeakCandy=lastCandy;
+            }
+           else{
+                int dist=i-lastPeakPos;
                 lastCandy=1;
-                int dist=i-lastPos;
-                 candy[i]+=dist-1;
-                if(candy[lastPos]<=dist)
-                   candy[lastPos]+=1;
+                if(dist==lastPeakCandy){
+                    dist+=1;
