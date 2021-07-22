3160818d3490b7c6345904a14b2a7af3623f2a31
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-07-22 23:14:09 +0530

    8 ms

--- solutions/1871. Jump Game VII.java
+++ solutions/1871. Jump Game VII.java
@@ -1,8 +1,8 @@
 class Solution {
-    public boolean canReach(String s, int minJump, int maxJump) {
+   /* public boolean canReach(String s, int minJump, int maxJump) {
         char[] chs = s.toCharArray();
         if(chs[0]!='0' || chs[chs.length-1]!='0') return false;
-        Queue<Integer> queue =  new LinkedList<>();
+        Queue<Integer> queue =  new LinkedList<>();
         queue.add(0);
         int index = 1;
         while(!queue.isEmpty() && index<chs.length){
@@ -12,17 +12,33 @@ class Solution {
                 
                 queue.poll();
                 continue;
-            } 
+            } 
             else if(chs[index]=='0' ){
                 
                 if(peekedIndex+minJump<=index && peekedIndex+maxJump>=index){
                     if(index==chs.length-1) return true;
                     queue.add(index);
                    
-                }
-            }
+                }
+            }
             index++;
-        }
+        }
         return false;
+    } 
+    */
+    // 8ms
+     public boolean canReach(String s, int minJ, int maxJ) {
+        int n = s.length(), pre = 0;
+        boolean[] dp = new boolean[n];
+        dp[0] = true;
+        for (int i = 1; i < n; ++i) {
+            if (i >= minJ && dp[i - minJ])
+                pre++;
+           
+            if (i > maxJ && dp[i - maxJ - 1])
+                pre--;
+            dp[i] = pre > 0 && s.charAt(i) == '0';
+        }
+        return dp[n - 1];
     }
 }
