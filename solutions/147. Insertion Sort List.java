2a46e814a54f62d8032232261dc214631b7fcaa3
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-07-27 23:42:15 +0530

    0 ms

---
+++ solutions/147. Insertion Sort List.java
@@ -0,0 +1,73 @@
+                break;
+            }
+            prev = prev.next;
+        }
+        ListNode tmp = prev.next;
+        prev.next = node;
+        node.next = tmp;
+        return isAddedBefore;
+        
+    }*/
+    //2ms
+   /* public ListNode insertionSortList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        
+        ListNode dummy=new ListNode(-1);
+        
+        dummy.next=head;
+        ListNode cur=head;
+        ListNode prev=null;
+        ListNode temp=null;
+        
+        while(cur!=null && cur.next!=null){
+            if(cur.val<=cur.next.val) {
+                cur=cur.next;
+            }else{
+                temp=cur.next;
+                cur.next=cur.next.next;
+                prev=dummy;
+                while(prev.next.val<temp.val){
+                    prev=prev.next;
+                }
+                temp.next=prev.next;
+                prev.next=temp;
+            }
+        }
+        return dummy.next;
+    }
+    */
+    // using merge sort technique 0 ms
+    public ListNode insertionSortList(ListNode head) {
+      if(head==null || head.next==null)
+            return head;
+            
+        // Finding the mid of the linked list and breaking the the list in two parts
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode mid=slow;
+        mid=mid.next;
+        slow.next=null;
+        
+        //recursive calls to sort the 2 seperated lists
+        ListNode a=insertionSortList(head);
+        ListNode b=insertionSortList(mid);
+        
+        //merging the 2 sorted lists
+        return merge(a,b);
+    }
+    public ListNode merge(ListNode a,ListNode b){
+        if(a==null) return b;
+        if(b==null) return a;
+        ListNode head;
+        ListNode cur;
+        if(a.val<b.val){
+            head=a;
+            cur=a;
+            a=a.next;
+        }else{
+            head=b;
+            cur=b;
