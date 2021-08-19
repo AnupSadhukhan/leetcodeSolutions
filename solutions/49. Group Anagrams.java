5b67a6a03f48a9f64f0240d0f9748fab58742157
Anup Sadhukhan <AnupSadhukhan@users.noreply.github.com>
2021-08-19 23:58:01 +0530

    38 ms

--- solutions/49. Group Anagrams.java
+++ solutions/49. Group Anagrams.java
@@ -1,33 +1,29 @@
 class Solution {
-    /* create hashmap for every word, if  hash key is already present in
-    the hashmap, add new word to the list value, else add key and add new 
-    list with cur word */
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> map=new HashMap<>();
+        Map<String,List<String>> map = new HashMap<>();
         for(String s : strs){
-            char[] chs=s.toCharArray();
-            int[] hash=new int[26];
+            int[] arr = new int[26];
             for(char c : s.toCharArray()){
-                hash[c-'a']++;
+                arr[c-'a']++;
             }
-            StringBuilder sb=new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             for(int i=0;i<26;i++){
-                if(hash[i]>0){
-                    char c=(char)(i+'a');
-                    sb.append(c+"-"+hash[i]);
+                if(arr[i]>0){
+                    char c = (char)(i+'a');
+                    sb.append(c+"-"+arr[i]);
                 }
             }
-            List<String> list=map.get(sb.toString());
+            List<String> list = map.get(sb.toString());
             if(list==null){
-                list=new ArrayList<>();
+                list = new ArrayList<>();
                 map.put(sb.toString(),list);
             }
             list.add(s);
             
         }
-        List<List<String>> result=new ArrayList<>();
-        for(String key: map.keySet()){
-            result.add(map.get(key));
+        List<List<String>> result = new ArrayList<>();
+        for(Map.Entry<String,List<String>> m : map.entrySet()){
+          result.add(m.getValue());   
         }
         return result;
     }
